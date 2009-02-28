74e986c94b9ffc3c02e768f9c77551adbd1e8ffa
Douglas Gilbert <dgilbert@interlog.com>
2009-02-28 21:44:24 +0000

    expand block limits VPD page
    git-svn-id: https://svn.bingwo.ca/repos/sdparm/trunk@108 c1e33020-0f26-4edd-b954-b49f5d3ab223

--- src/sdparm.c
+++ src/sdparm.c
@@ -76,7 +76,7 @@ static int map_if_lk24(int sg_fd, const char * device_name, int rw,
 
 #define MAX_DEV_NAMES 256
 
-static char * version_str = "1.04 20090223";
+static char * version_str = "1.04 20090228";
 
 
 static struct option long_options[] = {
