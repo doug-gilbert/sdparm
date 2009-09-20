9067fa16692ac342da789f8ebba3dfb0f754f566
Douglas Gilbert <dgilbert@interlog.com>
2009-09-20 16:06:43 +0000

    sg_pt_linux: include of <linux/kdev_t.h> if available
    git-svn-id: https://svn.bingwo.ca/repos/sdparm/trunk@127 c1e33020-0f26-4edd-b954-b49f5d3ab223

--- src/sdparm.c
+++ src/sdparm.c
@@ -76,7 +76,7 @@ static int map_if_lk24(int sg_fd, const char * device_name, int rw,
 
 #define MAX_DEV_NAMES 256
 
-static char * version_str = "1.04 20090916 [svn: r126]";
+static char * version_str = "1.04 20090920 [svn: r127]";
 
 
 static struct option long_options[] = {
