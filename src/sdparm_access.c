3d39e8ac2e76dc0fc74530387106fdd010215a1f
Douglas Gilbert <dgilbert@interlog.com>
2011-01-25 23:59:07 +0000

    LB provisioning + Block limits VPD pages to sbc3r26
    git-svn-id: https://svn.bingwo.ca/repos/sdparm/trunk@171 c1e33020-0f26-4edd-b954-b49f5d3ab223

--- src/sdparm_access.c
+++ src/sdparm_access.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005-2010 Douglas Gilbert.
+ * Copyright (c) 2005-2011 Douglas Gilbert.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
