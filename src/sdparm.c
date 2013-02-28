5604e671f1541914e54939551a30ff6a34e2ed18
Douglas Gilbert <dgilbert@interlog.com>
2013-02-28 19:50:26 +0000

    sync to rev 485 of sg3_utils' lib
    git-svn-id: https://svn.bingwo.ca/repos/sdparm/trunk@206 c1e33020-0f26-4edd-b954-b49f5d3ab223

--- src/sdparm.c
+++ src/sdparm.c
@@ -77,7 +77,7 @@ static int map_if_lk24(int sg_fd, const char * device_name, int rw,
 
 #define MAX_DEV_NAMES 256
 
-static char * version_str = "1.08 20130227 [svn: r205]";
+static char * version_str = "1.08 20130228 [svn: r206]";
 
 
 static struct option long_options[] = {
