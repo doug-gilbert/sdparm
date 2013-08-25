04190693b1200a4e99be390eb550c18c1b24a643
Douglas Gilbert <dgilbert@interlog.com>
2013-08-25 21:33:43 +0000

    point svn:externals to rev 510 of sg3_utils
    git-svn-id: https://svn.bingwo.ca/repos/sdparm/trunk@220 c1e33020-0f26-4edd-b954-b49f5d3ab223

--- src/sdparm.c
+++ src/sdparm.c
@@ -78,7 +78,7 @@ static int map_if_lk24(int sg_fd, const char * device_name, int rw,
 
 #define MAX_DEV_NAMES 256
 
-static const char * version_str = "1.09 20130820 [svn: r219]";
+static const char * version_str = "1.09 20130825 [svn: r220]";
 
 
 static struct option long_options[] = {
