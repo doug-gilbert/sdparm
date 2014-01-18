7280b9529ed5fec787ec37f8389a5d43f3479d38
Douglas Gilbert <dgilbert@interlog.com>
2014-01-18 03:28:10 +0000

    3PC VPD work; point svn:externals to rev 545 of sg3_utils
    git-svn-id: https://svn.bingwo.ca/repos/sdparm/trunk@230 c1e33020-0f26-4edd-b954-b49f5d3ab223

--- src/sdparm.c
+++ src/sdparm.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005-2013 Douglas Gilbert.
+ * Copyright (c) 2005-2014 Douglas Gilbert.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -78,7 +78,7 @@ static int map_if_lk24(int sg_fd, const char * device_name, int rw,
 
 #define MAX_DEV_NAMES 256
 
-static const char * version_str = "1.09 20131219 [svn: r229]";
+static const char * version_str = "1.09 20140117 [svn: r230]";
 
 
 static struct option long_options[] = {
