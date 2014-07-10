d8dcb4e69123c189c8b87222b596250b15ab9925
dougg <32302704+doug-gilbert@users.noreply.github.com>
2014-07-10 19:18:44 +0000

    point svn:externals to rev 591 of sg3_utils

--- src/sdparm.c
+++ src/sdparm.c
@@ -78,7 +78,7 @@ static int map_if_lk24(int sg_fd, const char * device_name, int rw,
 
 #define MAX_DEV_NAMES 256
 
-static const char * version_str = "1.09 20140608 [svn: r244]";
+static const char * version_str = "1.09 20140710 [svn: r245]";
 
 
 static struct option long_options[] = {
