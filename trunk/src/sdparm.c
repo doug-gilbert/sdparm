6918bc0ca60ae0516b394a899e8b36b81d31f38a
dougg <dougg@c1e33020-0f26-4edd-b954-b49f5d3ab223>
2014-09-23 15:48:44 +0000

    point svn:externals to rev 609 of sg3_utils; cygwin warning suppression

--- trunk/src/sdparm.c
+++ trunk/src/sdparm.c
@@ -78,7 +78,7 @@ static int map_if_lk24(int sg_fd, const char * device_name, int rw,
 
 #define MAX_DEV_NAMES 256
 
-static const char * version_str = "1.09 20140921 [svn: r250]";
+static const char * version_str = "1.09 20140923 [svn: r251]";
 
 
 static struct option long_options[] = {
