a489d243621cc6dafa626502dfc0149189f14dfa
dougg <dougg@c1e33020-0f26-4edd-b954-b49f5d3ab223>
2016-02-02 15:24:56 +0000

    fix sg_pr2serr.h header for C++

--- trunk/src/sdparm.c
+++ trunk/src/sdparm.c
@@ -78,7 +78,7 @@ static int map_if_lk24(int sg_fd, const char * device_name, int rw,
 #include "sg_pr2serr.h"
 #include "sdparm.h"
 
-static const char * version_str = "1.10 20160201 [svn: r275]";
+static const char * version_str = "1.10 20160202 [svn: r276]";
 
 
 #define MAX_DEV_NAMES 256
