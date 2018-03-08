51422721383e52d76d32a1f2b41d8b7b3d0f535d
Douglas Gilbert <dgilbert@interlog.com>
2018-03-08 03:36:15 +0000

    point svn:externals to rev 760 of sg3_utils
    git-svn-id: https://svn.bingwo.ca/repos/sdparm/trunk@308 c1e33020-0f26-4edd-b954-b49f5d3ab223

--- src/sdparm.c
+++ src/sdparm.c
@@ -80,7 +80,7 @@ static int map_if_lk24(int sg_fd, const char * device_name, bool rw,
 #include "sg_pr2serr.h"
 #include "sdparm.h"
 
-static const char * version_str = "1.11 20180227 [svn: r307]";
+static const char * version_str = "1.11 20180307 [svn: r308]";
 
 
 #define MAX_DEV_NAMES 256
