7214e54e2376619d742866c61b46b6ce7eac3fb4
doug-gilbert <32302704+doug-gilbert@users.noreply.github.com>
2018-03-22 00:06:26 +0100

    point svn:externals to rev 763 of sg3_utils

--- src/sdparm.c
+++ src/sdparm.c
@@ -80,7 +80,7 @@ static int map_if_lk24(int sg_fd, const char * device_name, bool rw,
 #include "sg_pr2serr.h"
 #include "sdparm.h"
 
-static const char * version_str = "1.11 20180316 [svn: r309]";
+static const char * version_str = "1.11 20180321 [svn: r310]";
 
 
 #define MAX_DEV_NAMES 256
