0659e36439ac1a0e1796a918ffd0d5d53bbf71f9
Douglas Gilbert <dgilbert@interlog.com>
2026-04-28 02:58:51 +0000

    cmake: experimental build alternative which can build deb and rpm packages with 'cpack .'
    git-svn-id: https://svn.bingwo.ca/repos/sdparm/trunk@396 c1e33020-0f26-4edd-b954-b49f5d3ab223

---
+++ cmake_del_artifacts.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+rm -rf \
+	CMakeCache.txt \
+	CMakeFiles \
+	CPackConfig.cmake \
+	CPackSourceConfig.cmake \
+	CMakeFiles \
+	cmake_install.cmake \
+	install_manifest.txt \
+	sdparm \
+	sdparm.8.gz \
+	sdparm_json.8.gz \
+	_CPack_Packages \
+	Makefile
+
