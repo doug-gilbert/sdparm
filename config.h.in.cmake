cb7f1e7a5a8eb0f895bb96adc7742d72df451160
Paul Evans <pevans@redhat.com>
2026-06-06 10:12:58 +0100

    cmake: achieve parity with autotools build
    Add missing feature checks, platform-specific defines and flags,
    and fix doc/CMakeLists.txt paths. Clean up config.h.in.cmake and
    add CPack RPM/DEB runtime dependencies. Bump minimum to 3.18.
    
    Signed-off-by: Paul Evans <pevans@redhat.com>

--- config.h.in.cmake
+++ config.h.in.cmake
@@ -1,35 +1,34 @@
 
 #cmakedefine SG_LIB_LINUX 1
+#cmakedefine SG_LIB_ANDROID 1
 #cmakedefine SG_LIB_FREEBSD 1
+#cmakedefine SG_LIB_NETBSD 1
+#cmakedefine SG_LIB_OPENBSD 1
 #cmakedefine SG_LIB_SOLARIS 1
-#cmakedefine SG_LIB_CYGWIN 1
+#cmakedefine SG_LIB_AIX 1
+#cmakedefine SG_LIB_WIN32 1
 #cmakedefine SG_LIB_MINGW 1
-#cmakedefine SG_LIB_AIX 1
-#cmakedefine SG_LIB_WINDOWS 1
-#cmakedefine SG_LIB_ABROID 1
-#cmakedefine SG_LIB_SOLARIS 1
-#cmakedefine SG_LIB_AIX 1
 #cmakedefine SG_LIB_MACOS 1
 
+#cmakedefine SG_LIB_BUILD_HOST "@SG_LIB_BUILD_HOST@"
+
+#cmakedefine HAVE_GETOPT_LONG 1
+#cmakedefine HAVE_NVME 1
 #cmakedefine HAVE_POSIX_MEMALIGN 1
-#cmakedefine IGNORE_FAST_LEBE 1
-#cmakedefine IGNORE_LINUX_BSG 1
-#cmakedefine IGNORE_NVME 1
-#cmakedefine SG_SCSI_STRINGS 1
+#cmakedefine HAVE_POSIX_FADVISE 1
+#cmakedefine HAVE_SYSCONF 1
+#cmakedefine HAVE_LSEEK64 1
+#cmakedefine HAVE___U64 1
 #cmakedefine HAVE_BYTESWAP_H 1
 #cmakedefine HAVE_LINUX_TYPES_H 1
 #cmakedefine HAVE_LINUX_BSG_H 1
 #cmakedefine HAVE_LINUX_KDEV_T_H 1
+#cmakedefine HAVE_LINUX_MAJOR_H 1
 
-#cmakedefine XXXXXXX 1
+#cmakedefine IGNORE_FAST_LEBE 1
+#cmakedefine IGNORE_LINUX_BSG 1
+#cmakedefine IGNORE_NVME 1
+#cmakedefine SG_SCSI_STRINGS 1
 
 #define BUILD_TIME "@BUILD_TIME@"
-
-// #define SG_LIB_LINUX @OS_LINUX@
-// #define SG_LIB_FREEBSD @OS_FREEBSD@
-// #cmakedefine01 SG_LIB_AIX @SG_LIB_AIX@
-
-// # This will generate a line in the output_file. Then in CMLists.txt:
-// #      set(FEATURE_COMMENT "//")
-// @FEATURE_COMMENT@#define OPTIONAL_SETTING 1
 
